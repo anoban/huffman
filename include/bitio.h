7c1598c97f5553b5c422dc7f568c28f79e27ffbd
anoban <2016sb117@univ.jfn.ac.lk>
2023-10-22 08:05:01 +0530

    ... :)

---
+++ include/bitio.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef __BITIO_H__
+    #define __BITIO_H__
+
+#endif // !__BITIO_H__
--- src/main.c
+++ src/main.c
@@ -2,4 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int wmain(_In_opt_ int argc, _In_opt_count_(argc) wchar_t* argv[]) { return EXIT_SUCCESS; }
+int wmain(_In_opt_ int argc, _In_opt_count_(argc) wchar_t* argv[]) { 
+	return EXIT_SUCCESS; 
+}
