0dcd5b074a574ed85bf0b5dfcba09aa620aa50d1
anoban <2016sb117@univ.jfn.ac.lk>
2023-10-24 19:03:09 +0530

    :)))

--- include/heap.h
+++ include/heap.h
@@ -8,7 +8,7 @@
 
 // Code improvised from Mastering Algorithms with C (1999) Kyle Loudon
 
-#define HPCAP 1024LLU
+#define HPCAP 1024Ui64
 
 // Many problems involve a programme to quickly determine the smallest or largest element from a collection
 // capable of frequent insertions and deletions.
@@ -45,8 +45,10 @@
 
 /*
                             (25)             <--- 3rd (Root/Top)
-                         /       \
-                        /         \
+                           /    \
+                          /      \
+                         /        \
+                        /          \
                       (20)         (22)      <--- 2nd
                      /    \        /  \
                     /      \      /    \
@@ -64,13 +66,25 @@
 // {25, 20, 22, 17, 19, 10, 12}:                 after traversing the 1st level
 // {25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18}: after traversing the 0th level
 
+// This design offers an inherent benefit where the parent node of a given node can easily be located by the 
+// offset (i - 1) / 2 where i is the offset of the select node.
+
+// E.g: What's the offset of node 19's parent?
+// Offset of node 19 is 4 (0 indexed array): i = 4
+// (4 - 1) integer division 2 = 1
+// node at offset 1 is 20, and it indeed is the parent of node 19.
+
+// And the offset of a given node with offset i's left child is 2i + 1
+// and the offset of the right child is 2i + 2
+// E.g. Take the node 20 for example, offset of node 20 is 1; i = 1
+// left child = 2 * 1 + 1 = 3 and right child = 2 * 1 + 2 = 4
+// 17 and 19 => correct!
+
 // Leftmost node must be the heaviest, and we do not care about the ordering of other nodes in the heap (array)
 
-
-static size_t inline __stdcall heapParentPos(_In_ const size_t pos) { return (pos - 1) / 2; }  // truncating division.
-static size_t inline __stdcall heapLeftPos(_In_ const size_t pos) { return (pos * 2) + 1; } 
-static size_t inline __stdcall heapRightPos(_In_ const size_t pos) { return (pos * 2) + 2; }
-
+static size_t inline __stdcall parentPos(_In_ const size_t child_pos) { return (child_pos - 1) / 2; } // truncating division.
+static size_t inline __stdcall leftChildPos(_In_ const size_t parent_pos) { return (parent_pos * 2) + 1; }
+static size_t inline __stdcall rightChildPos(_In_ const size_t parent_pos) { return (parent_pos * 2) + 2; }
 
 typedef struct heap {
         uint64_t count;     // number of nodes.
@@ -81,20 +95,22 @@ typedef struct heap {
                             // use malloc to allocate the tree and the nodes.
 }heap_t;
 
-static void inline heapInit(
-    _In_ heap_t* const restrict heap,
+static bool inline heapInit(
+    _Inout_ heap_t* const restrict heap,
     _In_ const int32_t(*predicate)(_In_reads_(1) const void* const restrict _this, _In_reads_(1) const void* const restrict _next),
     _In_ const void    (*clean)(_In_reads_(1) const void* const restrict memblock)
 ) {
     heap->count     = 0;
-    heap->capacity  = 0;
+    heap->capacity    = HPCAP;
     heap->fnptr_pred = predicate;
     heap->fnptr_clean = clean;
-    heap->tree      = NULL;
-    return;
+    // will initially allocate memory to store 1024 pointers in the tree.
+    if(!(heap->tree      = malloc(HPCAP * sizeof(uintptr_t)))) return false;
+    memset(heap->tree, 0U, HPCAP * sizeof(uintptr_t));
+    return true;
 }
 
-static void inline heapClean(_In_ heap_t* const restrict heap) {
+static void inline heapClean(_Inout_ heap_t* const restrict heap) {
     for (size_t i = 0; i < heap->count; ++i) free(heap->tree[i]); // free the heap allocated nodes.
     free(heap->tree);   // free the array containing pointers to heap allocated nodes.
     // two levels of heap allocation happens here!
@@ -102,24 +118,153 @@ static void inline heapClean(_In_ heap_t* const restrict heap) {
     return;
 }
 
-static bool inline heapPush(_In_ heap_t* const restrict heap, _In_ const void* const restrict data /* expects a heap allocated memory block (node) */) {
+static bool inline heapPush(_Inout_ heap_t* const restrict heap, _In_ const void* const restrict data /* expects a heap allocated memory block (node) to push in */) {
     void*  tmp    = NULL;
-    size_t curpos = 0, prevpos = 0;
+    size_t childpos = 0, parentpos = 0;
 
     // if the buffer doesn't have space for another pointer,
-    if (heap->count + 1 >= heap->capacity) {
+    if (heap->count + 1 > heap->capacity) {
         // ask for an additional 1024 * sizeof(uintptr_t) bytes. return false if the reallocation has failed.
+        // genuinely fancying an arena allocator here!
         if (!(tmp = realloc(heap->tree, (heap->count + HPCAP) * sizeof(uintptr_t)))) return false;
-        heap->tree = tmp;   // if reallocation was successful, reassign the new memory block.
+        heap->tree = tmp;   // if reallocation was successful, reassign the new memory block to tree.
     }
     
     heap->capacity          = heap->count + HPCAP;
-    // genuinely fancying an arena allocator here!
-    heap->tree[heap->count] = data;
+    
+    // Consider our previous tree:
+    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18 }
+    // let's try and push a node with weight 24
+    // after pushing we'd end up with an array like this, with the pushed node appended at the rightmost end.
+    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18, 24 }
+    // 24 is at offset 11, that makes it a child of node (i - 1) / 2 = 5, that is node 10.
 
+    /* Now the tree assumes a shape like this:
+    
+                               (25)               <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (22)      <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (10)  (12)   <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    / 
+               (15)   (7)(9)  (18)(24)            <--- 0th (Bottom)
+    */
 
+    // Since a child node cannot have a higher weight than a parent node in a top-heavy heap, this needs doctoring.
+    // To make this push comply to the heap data structure, we'll swap child and parents until we get to an acceptable structure.
+    
+    // First swap is between 24 and its parent 10;
+    /* Now the tree assumes a shape like this:
+
+                               (25)               <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (22)      <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (24)  (12)   <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(10)            <--- 0th (Bottom)
+    */
+    // Still we have 22 as parent of 24, swap these two again;
+    /* Now the tree assumes a shape like this:
+    * 
+                               (25)                <--- 3rd (Root/Top)
+                             /     \               
+                            /       \              
+                           /         \             
+                          /           \            
+                         /             \           
+                        /               \          
+                      (20)              (24)       <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (22)  (12)    <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(10)             <--- 0th (Bottom)
+    */
+    // {25, 20, 24, 17, 19, 22, 12, 15, 7, 9, 18, 10}
+    // Perfecto :)))
+
+    heap->tree[heap->count] = data; // insert the new node, it's not count + 1! offsets start at 0.
+    childpos                = heap->count;  // offset of the newly inserted node.
+    parentpos               = parentPos(childpos);  // offset of the new node's parent.
+
+    while ((childpos > 0) /* as long as we haven't reached the root node at offset 0 */ && heap->fnptr_pred(heap->tree[parentpos], heap->tree[childpos])) {
+        // and the weight of the child is greater than that of the parent. (two conditions needed to perform the swap)
+        
+        tmp = heap->tree[childpos];
+        heap->tree[parentpos] = heap->tree[childpos];
+        heap->tree[childpos]  = tmp;
+        tmp                   = NULL;
+
+        // prepare for the next swap in anticipation that the new positioning of the inserted node also needs a swap.
+        childpos              = parentpos;  // previous parent's offset now becomes the child's offset.
+        parentpos             = parentPos(childpos);    // find the parent of the current child's offset.
+    }
+    
+    heap->count++;  // increment the node count.
     return true;
 }
 
+
+static bool inline heapPop(_Inout_ heap_t* const restrict heap, _Inout_ void** restrict data /* popped out */) {
+    void * save = NULL, *tmp = NULL;
+    size_t childpos = 0, parentpos = 0;
+
+    // if the heap is empty,
+    if (!heap->count) return false;
+
+    // If the heap has only one node,
+    if (heap->count == 1) {
+        *data = heap->tree[0];  
+        heap->count--;
+        heapClean(heap);
+    }
+     
+    // {25, 20, 24, 17, 19, 22, 12, 15, 7, 9, 18, 10} ----------\
+                                                             // |  
+    // give up the node at the top (root node)s              // |
+    *data = heap->tree[0];                                   // |
+    heap->tree[0] = NULL;                                    // |
+    heap->count--;                                           // |
+                                                             // |
+    // {NULL, 20, 24, 17, 19, 22, 12, 15, 7, 9, 18, 10} <-------/
+
+    /* Now the tree looks like this:
+
+                             (NULL)                <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (24)       <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (22)  (12)    <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(10)             <--- 0th (Bottom)
+    */
+
+    // rearrange the tree.
+    // Move the last node in the array to the root's position, offset 0.
+
+}
 
 #endif // !__HEAP_H__
