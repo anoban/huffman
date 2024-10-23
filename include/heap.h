f9158f0eafe365195b6420e60ce5f5cbc5089ec4
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-24 04:15:33 +0530

    modified:   huffman.vcxproj 	modified:   include/bintree.h 	modified:   include/heap.h 	modified:   include/pqueue.h 	deleted:    tests/heap.cpp 	modified:   tests/tests.vcxproj 	modified:   tests/tests.vcxproj.filters

--- include/heap.h
+++ include/heap.h
@@ -4,78 +4,11 @@
 #define DEFAULT_HEAP_CAPACITY       1024LLU
 #define DEFAULT_HEAP_CAPACITY_BYTES (DEFAULT_HEAP_CAPACITY * sizeof(uintptr_t))
 
-// many problems demand determination of the smallest or largest element from a collection that's capable of frequent insertions and deletions
-// one way to meet this need is to keep the collection sorted all the time but sorting a collection at every insertion or deletion is expensive
-
-// fortunately this requirement does not insinuate that all the elements in a collection need to be sorted.
-// it's enough for the collection to just know the location of the smallest or the largest element, enabling fast access.
-// heaps and priority queue are adept for this purpose
-
-// heaps are organized trees (usually a binary tree) that allow easy access to the largest / smallest element.
-// the cost of creating and maintaining a tree is negligible compared to the performance overhead incurred by maintaining the container sorted all the time
-
-// in a heap, each child node has a weight smaller than its parent's, ergo it is the root node that has the largest weight in a heap
-// binary trees like these are top heavy, as we climb up the hierarchy of nodes, nodes will become more and more heavy (will have a larger weight)
-
-// trees can also be architectured to hold the heaviest nodes at the bottom and lightest nodes at the top, whereby the root node will be the one with the smallest weight
-// in that case, parent node will have a smaller weight compared to its children, hence making the tree bottom heavy
-
-// heaps are typically left balanced.
-// when new nodes are annexed to the tree at a given level, the tree grows from left to right.
-
-// an efficient way to implement left-balanced binary tree is to store the nodes in a contiguous array.
-// nodes are arranged in this array in the order we'd encounter them in a level traversal.
-
-// say that we have 11 leaf nodes with weights 15, 7, 9, 18, 10, 12, 17, 19, 20, 22 ans 25 to begin with.
-// NOTE: parent nodes here aren't created from the children!
-// THIS TREE ISN'T REPRESENTATIVE OF AN ORDERED HEAP. JUST AN EXEMPLAR FOR REPRESENTING A TREE USING AN ARRAY.
-
-/*
-                            (25)             <--- 3rd (Root/Top)
-                           /    \
-                          /      \
-                         /        \
-                        /          \
-                      (20)         (22)      <--- 2nd
-                     /    \        /  \
-                    /      \      /    \
-                  (17)     (19) (10)  (12)   <--- 1st
-                  /  \     /  \
-                 /    \   /    \
-               (15)   (7)(9)  (18)           <--- 0th (Bottom)
-*/
-
-// the array representation will be:
-// we move left -> right, through the hierarchies.
-
-/*
- {25}:                                          (after traversing the 3rd level)
- {25, 20, 22}:                                  (after traversing the 2nd level)
- {25, 20, 22, 17, 19, 10, 12}:                  (after traversing the 1st level)
- {25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18}:  (after traversing the 0th level)
-*/
-
-// this design offers an inherent benefit that the parent node of a given node can easily be located by the
-// offset (i - 1) / 2 where i is the offset of the chosen child node.
-
-// E.g: what's the offset of node 19's parent?
-// offset of node 19 is 4 (0 indexed array): i = 4
-// (4 - 1) / 2 = 1 (integer division)
-// node at offset 1 is 20, and it indeed is the parent of node 19.
-
-// and the offset of a given node with offset i's left child is 2i + 1 and its right child is 2i + 2
-// E.g. Take the node 20 for example, offset of node 20 is 1; i = 1
-// left child = 2 * 1 + 1 = 3 and right child = 2 * 1 + 2 = 4
-// 17 and 19 => correct!
-
-// leftmost node must be the heaviest, and we do not care about the ordering of other nodes in the heap (array)
-
-typedef struct _heap {     // heap
-        uint32_t count;    // number of nodes.
-        uint32_t capacity; // number of nodes the heap can hold before requiring a reallocation.
+typedef struct _heap { // heap
+        uint32_t count;
+        uint32_t capacity;
         bool (*predptr)(_In_ const void* const, _In_ const void* const);
-        void** tree; // a heap allocated array containing pointers to heap allocated nodes.
-                     // use malloc to allocate the tree and the nodes.
+        void** tree;
 } heap_t;
 
 static_assert(sizeof(heap_t) == 24);
@@ -83,13 +16,6 @@ static_assert(offsetof(heap_t, count) == 0);
 static_assert(offsetof(heap_t, capacity) == 4);
 static_assert(offsetof(heap_t, predptr) == 8);
 static_assert(offsetof(heap_t, tree) == 16);
-
-/*
-// must return true whenever a swap is needed.
-static inline bool __cdecl predicate(const void* const  child, const void* const  parent){
-    retrun child->comparable > parent->comparable ? true : false;
-}
-*/
 
 static inline bool __cdecl heap_init(
     _Inout_ heap_t* const restrict heap, _In_ bool (*const predicate)(_In_ const void* const, _In_ const void* const)
@@ -99,7 +25,6 @@ static inline bool __cdecl heap_init(
 
     // NOLINTNEXTLINE(bugprone-multi-level-implicit-pointer-conversion)
     heap->tree = _CXX_COMPAT_REINTERPRET_CAST(void**, malloc(DEFAULT_HEAP_CAPACITY_BYTES));
-    // will allocate memory to store DEFAULT_HEAP_CAPACITY number of pointers in the tree.
 
     if (!heap->tree) {
         fwprintf_s(stderr, L"memory allocation error inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
@@ -107,7 +32,7 @@ static inline bool __cdecl heap_init(
     }
 
     heap->count    = 0;
-    heap->capacity = DEFAULT_HEAP_CAPACITY; // this is the number of pointers, NOT BYTES
+    heap->capacity = DEFAULT_HEAP_CAPACITY;
     heap->predptr  = predicate;
 
     memset(heap->tree, 0U, DEFAULT_HEAP_CAPACITY_BYTES); // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
@@ -118,16 +43,15 @@ static inline void __cdecl heap_clean(_Inout_ heap_t* const restrict heap) {
     assert(heap);
 
     for (unsigned i = 0; i < heap->count; ++i) {
-        free(heap->tree[i]); // free the heap allocated nodes.
-                             // heap->tree[i] = NULL;   // redundant
+        free(heap->tree[i]);
+        // heap->tree[i] = NULL;   // redundant
     }
-    free(heap->tree);                 // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
-    memset(heap, 0U, sizeof(heap_t)); // zero out the struct
+    free(heap->tree); // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
+    memset(heap, 0U, sizeof(heap_t));
 }
 
 static inline bool __cdecl heap_push(
-    _Inout_ heap_t* const restrict heap,
-    _In_ const void* const restrict pushed /* expects a heap allocated memory block to push into the heap */
+    _Inout_ heap_t* const restrict heap, _In_ const void* const restrict pushed
 
 ) {
     assert(heap);
@@ -142,84 +66,15 @@ static inline bool __cdecl heap_push(
         if (!(_temp_tree = _CXX_COMPAT_REINTERPRET_CAST(
                   void**, realloc(heap->tree, heap->capacity * sizeof(uintptr_t) + DEFAULT_HEAP_CAPACITY_BYTES)
               ))) {
-            // ask for an additional DEFAULT_HEAP_CAPACITY_BYTES bytes.
-            // at this point, heap->tree is valid and points to the old buffer
             fwprintf_s(stderr, L"memory reallocation error inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
-            return false; // return false if the reallocation fails.
+            return false;
         }
 
-        heap->tree      = _temp_tree;            // if reallocation was successful, reassign the new memory block to tree.
-        heap->capacity += DEFAULT_HEAP_CAPACITY; // update the capacity
+        heap->tree      = _temp_tree;
+        heap->capacity += DEFAULT_HEAP_CAPACITY;
     }
 
-    // consider our previous tree:
-    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18 }
-    // let's try and push a node with weight 24
-    // after pushing we'd end up with an array like this, with the pushed node appended at the rightmost end.
-    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18, 24 }
-    // 24 is at offset 11, that makes it a child of node (i - 1) / 2 = 5, that is node 10.
-
-    /* now the tree assumes a shape like this:
-
-                               (25)               <--- 3rd (Root/Top)
-                             /     \
-                            /       \
-                           /         \
-                          /           \
-                         /             \
-                        /               \
-                      (20)              (22)      <--- 2nd
-                     /    \             /  \
-                    /      \           /    \
-                  (17)     (19)      (10)  (12)   <--- 1st
-                  /  \     /  \      /
-                 /    \   /    \    /
-               (15)   (7)(9)  (18)(24)            <--- 0th (Bottom)
-    */
-
-    // since a child node cannot have a bigger weight than its parent node in a top-heavy heap, this needs reordering.
-    // to make this push comply with the heap requisites, we'll swap child and parents until we get to an acceptable structure.
-
-    // first swap is between 24 and its parent 10;
-    /* now the tree assumes a shape like this:
-
-                               (25)               <--- 3rd (Root/Top)
-                             /     \
-                            /       \
-                           /         \
-                          /           \
-                         /             \
-                        /               \
-                      (20)              (22)      <--- 2nd
-                     /    \             /  \
-                    /      \           /    \
-                  (17)     (19)      (24)  (12)   <--- 1st
-                  /  \     /  \      /
-                 /    \   /    \    /
-               (15)   (7)(9)  (18)(10)            <--- 0th (Bottom)
-    */
-
-    // still we have 22 as parent of 24, swap these two again;
-    /* now the tree assumes a shape like this:
-
-                               (25)                <--- 3rd (Root/Top)
-                             /     \
-                            /       \
-                           /         \
-                          /           \
-                         /             \
-                        /               \
-                      (20)              (24)       <--- 2nd
-                     /    \             /  \
-                    /      \           /    \
-                  (17)     (19)      (22)  (12)    <--- 1st
-                  /  \     /  \      /
-                 /    \   /    \    /
-               (15)   (7)(9)  (18)(10)             <--- 0th (Bottom)
-    */
-    // {25, 20, 24, 17, 19, 22, 12, 15, 7, 9, 18, 10}
-    // perfecto :)))
-
+    
     heap->count++;                                                       // increment the node count.
     heap->tree[heap->count - 1] = _CXX_COMPAT_CONST_CAST(void*, pushed); // hook in the new node
 
--- include/pqueue.h
+++ include/pqueue.h
@@ -4,11 +4,78 @@
 #define DEFAULT_PQUEUE_CAPACITY       1024LLU
 #define DEFAULT_PQUEUE_CAPACITY_BYTES (DEFAULT_PQUEUE_CAPACITY * sizeof(uintptr_t))
 
-typedef struct _pqueue { // priority queue
-        uint32_t count;
-        uint32_t capacity;
+// many problems demand determination of the smallest or largest element from a collection that's capable of frequent insertions and deletions
+// one way to meet this need is to keep the collection sorted all the time but sorting a collection at every insertion or deletion is expensive
+
+// fortunately this requirement does not insinuate that all the elements in a collection remain sorted.
+// it's enough for the collection to just know the location of the smallest or the largest element, hence enabling rapid access to that element.
+// heaps and priority queue are adept for this purpose
+// a priority queue can be implemented easily by encapsulating a heap
+
+// heaps are organized trees (usually a binary tree) that allow easy access to the largest / smallest element.
+// the cost of creating and maintaining a tree is negligible compared to the performance overhead incurred by maintaining the container sorted all the time
+
+// in a heap, each child node has a weight smaller than its parent's, ergo it is the root node that has the largest weight in a heap
+// binary trees like these are top heavy, as we climb up the hierarchy of nodes, nodes will become more and more heavy (will have a larger weight)
+
+// trees can also be engineered to hold the heaviest nodes at the bottom and lightest nodes at the top, whereby the root node will be the one with the smallest weight
+// in that case, parent node will have a smaller weight compared to its children, hence making the tree bottom heavy
+
+// heaps are typically left balanced.
+// when new nodes are annexed to the tree at a given level, the tree grows from left to right.
+
+// an efficient way to implement left-balanced binary tree is to store the nodes in a contiguous array.
+// nodes are arranged in this array in the order we'd encounter them in a level traversal.
+
+// say that we have 11 leaf nodes with weights 15, 7, 9, 18, 10, 12, 17, 19, 20, 22 ans 25 to begin with.
+// NOTE: parent nodes here aren't created from the children!
+// THIS TREE ISN'T REPRESENTATIVE OF AN ORDERED HEAP. JUST AN EXEMPLAR FOR REPRESENTING A TREE USING AN ARRAY.
+
+/*
+                            (25)             <--- 3rd (Root/Top)
+                           /    \
+                          /      \
+                         /        \
+                        /          \
+                      (20)         (22)      <--- 2nd
+                     /    \        /  \
+                    /      \      /    \
+                  (17)     (19) (10)  (12)   <--- 1st
+                  /  \     /  \
+                 /    \   /    \
+               (15)   (7)(9)  (18)           <--- 0th (Bottom)
+*/
+
+// the array representation will be:
+// we move left -> right, through the hierarchies.
+
+/*
+ {25}:                                          (after traversing the 3rd level)
+ {25, 20, 22}:                                  (after traversing the 2nd level)
+ {25, 20, 22, 17, 19, 10, 12}:                  (after traversing the 1st level)
+ {25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18}:  (after traversing the 0th level)
+*/
+
+// this design offers an inherent benefit that the parent node of a given node can easily be located by the
+// offset (i - 1) / 2 where i is the offset of the chosen child node.
+
+// E.g: what's the offset of node 19's parent?
+// offset of node 19 is 4 (0 indexed array): i = 4
+// (4 - 1) / 2 = 1 (integer division)
+// node at offset 1 is 20, and it indeed is the parent of node 19.
+
+// and the offset of a given node with offset i's left child is 2i + 1 and its right child is 2i + 2
+// E.g. Take the node 20 for example, offset of node 20 is 1; i = 1
+// left child = 2 * 1 + 1 = 3 and right child = 2 * 1 + 2 = 4
+// 17 and 19 => correct!
+
+// leftmost node must be the heaviest, and we do not care about the ordering of other nodes in heaps & priority queues
+
+typedef struct _pqueue {   // priority queue
+        uint32_t count;    // number of nodes.
+        uint32_t capacity; // number of nodes the heap can hold before requiring a reallocation.
         bool (*predptr)(_In_ const void* const, _In_ const void* const);
-        void** tree;
+        void** tree; // a heap allocated array containing pointers to heap allocated nodes. use malloc to allocate the tree and the nodes.
 } PQueue;
 
 static_assert(sizeof(PQueue) == 24);
@@ -17,6 +84,13 @@ static_assert(offsetof(PQueue, capacity) == 4);
 static_assert(offsetof(PQueue, predptr) == 8);
 static_assert(offsetof(PQueue, tree) == 16);
 
+/*
+// must return true whenever a swap is needed.
+static inline bool __cdecl predicate(const void* const  child, const void* const  parent){
+    retrun child->comparable > parent->comparable ? true : false;
+}
+*/
+
 // using pascal case for queues
 static inline bool __cdecl PQueueInit(
     _Inout_ PQueue* const pqueue, _In_ bool (*const predicate)(_In_ const void* const child, _In_ const void* const parent)
@@ -24,31 +98,32 @@ static inline bool __cdecl PQueueInit(
     assert(pqueue);
     assert(predicate);
 
-    pqueue->tree = _CXX_COMPAT_REINTERPRET_CAST( // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
-        void**,
-        malloc(DEFAULT_PQUEUE_CAPACITY_BYTES)
-    );
+    // NOLINTNEXTLINE(bugprone-multi-level-implicit-pointer-conversion)
+    pqueue->tree = _CXX_COMPAT_REINTERPRET_CAST(void**, malloc(DEFAULT_PQUEUE_CAPACITY_BYTES));
+    // will allocate memory to store DEFAULT_PQUEUE_CAPACITY number of pointers in the tree.
 
     if (!pqueue->tree) {
-        fwprintf_s(stderr, L"memory allocation error inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
+        fwprintf_s(stderr, L"malloc failed inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
         return false;
     }
 
     pqueue->count    = 0;
-    pqueue->capacity = DEFAULT_PQUEUE_CAPACITY;
+    pqueue->capacity = DEFAULT_PQUEUE_CAPACITY; // this is the number of pointers, NOT BYTES
     pqueue->predptr  = predicate;
     memset(pqueue->tree, 0U, DEFAULT_PQUEUE_CAPACITY_BYTES); // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
     return true;
 }
 
 static inline void __cdecl PQueueClean(_Inout_ PQueue* const pqueue) {
-    for (size_t i = 0; i < pqueue->count; ++i) free(pqueue->tree[i]);
-    free(pqueue->tree); // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
-    memset(pqueue, 0U, sizeof(PQueue));
+    for (size_t i = 0; i < pqueue->count; ++i) free(pqueue->tree[i]); // free the heap allocated nodes.
+    free(pqueue->tree);                                               // NOLINT(bugprone-multi-level-implicit-pointer-conversion)
+    memset(pqueue, 0U, sizeof(PQueue));                               // zero out the struct
 }
 
 // enqueue
-static inline bool __cdecl PQueuePush(_Inout_ PQueue* const pqueue, _In_ void* const data) {
+static inline bool __cdecl PQueuePush(
+    _Inout_ PQueue* const pqueue, _In_ void* const data /* expects a heap allocated memory block to push into the heap */
+) {
     assert(pqueue);
     assert(data);
 
@@ -56,16 +131,91 @@ static inline bool __cdecl PQueuePush(_Inout_ PQueue* const pqueue, _In_ void* c
     size_t _childpos = 0, _parentpos = 0;          // NOLINT(readability-isolate-declaration)
 
     if (pqueue->count + 1 > pqueue->capacity) {
+        // ask for an additional DEFAULT_PQUEUE_CAPACITY_BYTES bytes.
+
         // NOLINTNEXTLINE(bugprone-assignment-in-if-condition, bugprone-multi-level-implicit-pointer-conversion)
         if (!(_temp_tree = _CXX_COMPAT_REINTERPRET_CAST(
-                  void**, realloc(pqueue->tree, pqueue->capacity * sizeof(uintptr_t) + DEFAULT_PQUEUE_CAPACITY_BYTES)
+                  void**,
+                  realloc(
+                      pqueue->tree,
+                      (pqueue->capacity * sizeof(uintptr_t)) /* size of the existing buffer in bytes */ + DEFAULT_PQUEUE_CAPACITY_BYTES
+                  )
               ))) {
-            fwprintf_s(stderr, L"memory reallocation error inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
-            return false;
+            fwprintf_s(stderr, L"realloc failed inside %s @LINE: %d\n", __FUNCTIONW__, __LINE__);
+            // at this point, heap->tree is still valid and points to the old buffer
+            return false; // return false if reallocation fails.
         }
-        pqueue->tree      = _temp_tree;
-        pqueue->capacity += DEFAULT_PQUEUE_CAPACITY;
+        pqueue->tree      = _temp_tree;              // if reallocation was successful, reassign the new memory block to tree.
+        pqueue->capacity += DEFAULT_PQUEUE_CAPACITY; // update the capacity
     }
+
+    // consider our previous tree:
+    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18 }
+    // let's try and push a node with weight 24
+    // after pushing we'd end up with an array like this, with the pushed node appended at the rightmost end.
+    // { 25, 20, 22, 17, 19, 10, 12, 15, 07, 09, 18, 24 }
+    // 24 is at offset 11, that makes it a child of node (i - 1) / 2 = 5, that is node 10.
+
+    /* now the tree assumes a shape like this:
+
+                               (25)               <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (22)      <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (10)  (12)   <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(24)            <--- 0th (Bottom)
+    */
+
+    // since a child node cannot have a bigger weight than its parent node in a top-heavy heap, this needs reordering.
+    // to make this push comply with the heap requisites, we'll swap child and parents until we get to an acceptable structure.
+
+    // first swap is between 24 and its parent 10;
+    /* now the tree assumes a shape like this:
+
+                               (25)               <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (22)      <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (24)  (12)   <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(10)            <--- 0th (Bottom)
+    */
+
+    // still we have 22 as parent of 24, swap these two again;
+    /* now the tree assumes a shape like this:
+
+                               (25)                <--- 3rd (Root/Top)
+                             /     \
+                            /       \
+                           /         \
+                          /           \
+                         /             \
+                        /               \
+                      (20)              (24)       <--- 2nd
+                     /    \             /  \
+                    /      \           /    \
+                  (17)     (19)      (22)  (12)    <--- 1st
+                  /  \     /  \      /
+                 /    \   /    \    /
+               (15)   (7)(9)  (18)(10)             <--- 0th (Bottom)
+    */
+    // {25, 20, 24, 17, 19, 22, 12, 15, 7, 9, 18, 10}
+    // perfecto :)))
 
     pqueue->count++;
     pqueue->tree[pqueue->count - 1] = _CXX_COMPAT_CONST_CAST(void*, data);
