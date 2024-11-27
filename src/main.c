b822b41a4b10e478914a9fd83243f272853e8035
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-27 20:34:19 +0530

    added more media

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ static unsigned long long frequencies[256] = { 0 };
 
 int wmain(_In_opt_ [[maybe_unused]] int argc, _In_opt_count_(argc) [[maybe_unused]] wchar_t* argv[]) {
     unsigned                   fsize = 0;
-    const unsigned char* const fbuff = open(L"./media/sqlite3.dll", &fsize);
+    const unsigned char* const fbuff = open(L"./media/middlemarch.txt", &fsize);
     assert(fbuff);
 
     scan_frequencies(fbuff, fsize, frequencies); // populate the frequency table
@@ -13,7 +13,7 @@ int wmain(_In_opt_ [[maybe_unused]] int argc, _In_opt_count_(argc) [[maybe_unuse
     pqueue_t prqueue = { 0 };
     // pqueue_init(&prqueue, );
 
-    for (unsigned i = 0; i < 256; ++i) wprintf_s(L"%4u   %10llu\n", i, frequencies[i]);
+    for (unsigned i = 0; i < 256; ++i) wprintf_s(L"%4u (%c)  %10llu\n", i, i, frequencies[i]);
 
     return EXIT_SUCCESS;
 }
